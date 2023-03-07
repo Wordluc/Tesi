022897bc92b4bdc14b36bd00d656bd4ae676eec9
Wordluc <40872910+Wordluc@users.noreply.github.com>
2023-03-07 15:25:29 +0100

    .

--- Game/Assets/shoot.cs
+++ Game/Assets/shoot.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class shoot : MonoBehaviour
 {
     public GameObject sampleBullet;
-    public float pressLevels;
     public float mTimeShoot;
     public float timeShoot=0f;
      public string command;
      public GameObject gun;
-     public Vector3 originalP;
+     private Vector3 originalP;
      public float resistThrill;
      public float forceThrill;
+     public float distant;
     void Start()
     {
        originalP=gun.transform.localPosition;
@@ -20,19 +20,24 @@ public class shoot : MonoBehaviour
          
           
         }
-    void Update()
+     void FixedUpdate()
     {  
         float rt=Input.GetAxis(command);//in modo da poter usare sia controller xbox che playStation
         gun.transform.localPosition+=(originalP-gun.transform.localPosition)*resistThrill*Time.deltaTime;
         if(rt>0f && SetUp.command){
-             float t=(int)(pressLevels*rt);
-             if(timeShoot>=mTimeShoot/t){
+             if(timeShoot>=mTimeShoot){
                timeShoot=0f; 
-               GameObject o=Instantiate(sampleBullet,transform.position,transform.rotation);
-               gun.transform.localPosition+=Vector3.forward*forceThrill*Time.deltaTime;
+                 if (Physics.Raycast(this.transform.position,  transform.forward,distant))
+                 {
+                    Debug.Log("Did Hit");
+                 }else
+                 Debug.Log("NODid Hit");
+               //GameObject o=Instantiate(sampleBullet,transform.position,transform.rotation);
+               //gun.transform.localPosition+=Vector3.forward*forceThrill*Time.deltaTime;
               
            }
            timeShoot+=Time.deltaTime;
+         
         }else
             timeShoot=0;
 
